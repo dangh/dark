87353c8ab5fd49f56e468e5ab9776c33c44f2444
Dang <huynhminhdang@gmail.com>
2024-11-23 13:59:51 +0800

    observer

--- dark.swift
+++ dark.swift
@@ -1,7 +1,17 @@
-import Cocoa;
+import Cocoa
 
-if (NSApplication.shared.effectiveAppearance.name == NSAppearance.Name.aqua) {
-    print("light");
-} else {
-    print("dark");
+func printAppearance() -> String {
+    if NSApplication.shared.effectiveAppearance.bestMatch(from: [.darkAqua, .aqua]) == .darkAqua {
+        return "dark"
+    } else {
+        return "light"
+    }
 }
+
+DistributedNotificationCenter.default().addObserver(forName: Notification.Name("AppleInterfaceThemeChangedNotification"), object: nil, queue: nil) { _ in
+    printAppearance()
+}
+
+printAppearance()
+
+NSApplication.shared.run()
